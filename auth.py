370afbc2dbb8b424ba4a3e3702f83373744c4e28
Vishal <67873373+vishalchepuri@users.noreply.github.com>
2022-06-24 19:33:06 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,14 +1,5 @@
 import pyrebase
-config = {
-        "apiKey": "AIzaSyDLKJlcPE55UnP6iN1ziuiYEoRVEUPToTA",
-        "authDomain": "learning-54b3e.firebaseapp.com",
-        "databaseURL": "https://learning-54b3e-default-rtdb.firebaseio.com",
-        "projectId": "learning-54b3e",
-        "storageBucket": "learning-54b3e.appspot.com",
-        "messagingSenderId": "290059634862",
-        "appId": "1:290059634862:web:c56a7c7c8c5f1367ec053f",
-        "measurementId": "G-23D5VEQXSH"
-    }
+
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
